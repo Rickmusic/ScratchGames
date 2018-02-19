f35e8fa79e9e3a477437e5ca82a1135defeec1b7
Kayleigh Kernaghan <26681805+AylaJK@users.noreply.github.com>
2018-02-19 00:07:34 -0700

    Added Linter and Formater

--- public/js/home.js
+++ public/js/home.js
@@ -1,11 +1,11 @@
-$(function () {
-    $.ajax({
-        url: "/user",
-        success: function (result) {
-            $('#displayname').text(result.displayname);
-        },
-        error: function (obj, status, textStatus) {
-            $('body').append($('p').text('Ajax Failed'));
-        }
-    });
+$(function() {
+  $.ajax({
+    url: '/user',
+    success: function(result) {
+      $('#displayname').text(result.displayname);
+    },
+    error: function(obj, status, textStatus) {
+      $('body').append($('p').text('Ajax Failed (Status ' + textStatus + ')'));
+    },
+  });
 });
