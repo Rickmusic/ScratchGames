b7e81c1a963c4c6013657c9976ea94e999e32ad4
Kayleigh Kernaghan <26681805+AylaJK@users.noreply.github.com>
2018-02-20 20:50:54 -0700

    Seperate Auth Routing into it's own file

---
+++ app/router/auth.js
@@ -0,0 +1,59 @@
+'use strict';
+
+let express = require('express');
+let router = express.Router();
+
+let path = require('path');
+
+let passport = require('../passport');
+
+let rootdir = { root: path.join(__dirname, '../../') };
+
+
+router.get('/login', function(req, res) {
+  if (req.query.redirect) {
+    req.session.return_to = req.query.redirect;
+  }
+  res.sendFile(path.join('public/html/login.html'), rootdir);
+  //res.redirect('/login.html?redirect=' + req.query.redirect);
+});
+
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/home',
+    failureRedirect: '/login',
+  })
+);
+
+router.post('/login/ajax', function(req, res, next) {
+  passport.authenticate('local', function(err, user, info) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.send({
+        success: false,
+        message: info.message ? info.message : 'Authentication Failed',
+      });
+    }
+    req.logIn(user, function(err) {
+      if (err) {
+        return next(err);
+      }
+      let redirect = req.session.return_to ? req.session.return_to : '/home';
+      delete req.session.return_to;
+      return res.send({
+        success: true,
+        redirect: redirect,
+      });
+    });
+  })(req, res, next);
+});
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+module.exports = router;
