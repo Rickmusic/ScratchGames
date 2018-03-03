3b8d801fcf72db1aab27cabc84502a7524f3bddc
Kayleigh Kernaghan <26681805+AylaJK@users.noreply.github.com>
2018-03-03 16:45:11 -0700

    Start styling home page for demo.
    Nginx redirects index to login.
    Home page added some styling.
    Moved where profile.js was putting user display name.

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,8 +2,8 @@ $(function() {
   $.ajax({
     url: '/info',
     success: function(result) {
+      $('#username').append(result.displayname);
       $('#profile').append('ID: ' + result.userid + '<br/>');
-      $('#profile').append('Name: ' + result.displayname + '<br>');
       $('#profile').append('Account Status: ' + result.status + '<br/>');
     },
     error: function(obj, status, textStatus) {
