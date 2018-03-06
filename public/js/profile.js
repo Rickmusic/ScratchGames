f5f712980667a03df2b6d75d2d93a6e7a12378f3
Kayleigh Kernaghan <26681805+AylaJK@users.noreply.github.com>
2018-03-05 22:53:00 -0700

    Basic Styling of Home Page for Demo

--- public/js/profile.js
+++ public/js/profile.js
@@ -3,8 +3,8 @@ $(function() {
     url: '/info',
     success: function(result) {
       $('#username').append(result.displayname);
-      $('#profile').append('ID: ' + result.userid + '<br/>');
-      $('#profile').append('Account Status: ' + result.status + '<br/>');
+      $('#profile').append($('<div />').html($('<p />').text('ID: ' + result.userid)));
+      $('#profile').append($('<div />').html($('<p />').text('Account Status: ' + result.status)));
     },
     error: function(obj, status, textStatus) {
       $('body').append($('p').text('Ajax Failed (Status ' + textStatus + ')'));
