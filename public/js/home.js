e7f14e7c3c4528c256d137d537332bd75009d5ea
Austin Wattling <Austin Wattling@CINDY>
2018-03-06 21:01:49 -0700

    Trial set of inner html created. Anything within the div with id change should be placed in external file.

--- public/js/home.js
+++ public/js/home.js
@@ -3,7 +3,10 @@ $(document).ready(function($) {
     // Adding Accordian Handler to accordians //
     $('.accord').find('.accordbtn').click(function(){
         $(this).next().slideToggle('fast');
+    });
+    $('.accord').find('.accordbtn2').click(function(){
+        $(this).next().slideToggle('fast');
         //Hide the other panels
-        // $(".accordcon").not($(this).next()).slideUp('fast');
+        $(".accordcon").not($(this).next()).slideUp('fast');
     });
 });
