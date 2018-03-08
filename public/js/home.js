5857a12f1f3cc6af93ae4f33dc9710aef37a9e4d
Austin Wattling <Austin Wattling@CINDY>
2018-03-07 18:00:01 -0700

    Created Popup dialog for create game.

--- public/js/home.js
+++ public/js/home.js
@@ -1,12 +1,20 @@
 // Onload Function //
 $(document).ready(function($) {
-    // Adding Accordian Handler to accordians //
+    // Adding Accordian Handler to side accordians //
     $('.accord').find('.accordbtn').click(function(){
         $(this).next().slideToggle('fast');
     });
+    // Adding Accordian Handler to bottom accordians //
     $('.accord').find('.accordbtn2').click(function(){
         $(this).next().slideToggle('fast');
         //Hide the other panels
         $(".accordcon").not($(this).next()).slideUp('fast');
+    });
+    // Handling press of create lobby button //
+    $('#createbtn').click(function(){
+        $('.modal').css("display", "block");
+    });
+    $('.close').click(function(){
+        $('.modal').css("display", "none");
     });
 });
