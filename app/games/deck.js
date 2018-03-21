a8d9c702a325f5d8bf51d311db2bfbd54997c9f2
Kayleigh Kernaghan <26681805+AylaJK@users.noreply.github.com>
2018-03-21 15:21:40 -0600

    Linting Server Files

--- app/games/deck.js
+++ app/games/deck.js
@@ -1,39 +1,39 @@
+'use strict';
+
 class Deck {
-	
-	constructor() {
-		this.cardsAvailable = [];
-		var suits = ["C", "H", "S", "D"];
-		for (var s = 0; s < 4; s++) {
-			for (var c = 1; c <= 13; c++) {
-				this.cardsAvailable.push({suit: suits[s], num: c});
-			}
-		}
-	}
-	dealCards(numberCards, numberPlayers, options=[]) {
-		var players = [];
-		for (i = 0; i < numberPlayers; i++) {
-			players.push([]);
-		}
-		
-		var cardsDealt = 0;
-		while (cardsDealt <= numberCards && this.cardsAvailable.length > 0) {
-			
-			for (var i in players) {
-				var player = players[i];
-				var index = Math.floor(Math.random()*this.cardsAvailable.length);
-				var card = this.cardsAvailable[index];
-				this.cardsAvailable.splice(index, 1);
-				player.push(card)
-			}
-			cardsDealt += 1;
-		}
-		return players
-	}
-	pickOne() {
-		var index = Math.floor(Math.random()*this.cardsAvailable.length);
-		var card = this.cardsAvailable[index];
-		this.cardsAvailable.splice(index, 1);
-		return card;
-	}
+  constructor() {
+    this.cardsAvailable = [];
+    let suits = ['C', 'H', 'S', 'D'];
+    for (let s = 0; s < 4; s++) {
+      for (let c = 1; c <= 13; c++) {
+        this.cardsAvailable.push({ suit: suits[s], num: c });
+      }
+    }
+  }
+  dealCards(numberCards, numberPlayers, options = []) {
+    let players = [];
+    for (let i = 0; i < numberPlayers; i++) {
+      players.push([]);
+    }
+
+    let cardsDealt = 0;
+    while (cardsDealt <= numberCards && this.cardsAvailable.length > 0) {
+      for (let i in players) {
+        let player = players[i];
+        let index = Math.floor(Math.random() * this.cardsAvailable.length);
+        let card = this.cardsAvailable[index];
+        this.cardsAvailable.splice(index, 1);
+        player.push(card);
+      }
+      cardsDealt += 1;
+    }
+    return players;
+  }
+  pickOne() {
+    let index = Math.floor(Math.random() * this.cardsAvailable.length);
+    let card = this.cardsAvailable[index];
+    this.cardsAvailable.splice(index, 1);
+    return card;
+  }
 }
 module.exports = Deck;
--- app/games/gofish/index.js
+++ app/games/gofish/index.js
@@ -1,54 +1,54 @@
-var GoFish = require("./game");
+'use strict';
 
+let GoFish = require('./game');
 
+module.exports = function(app, io) {
+  let game = new GoFish();
+  io.on('connection', function(socket) {
+    // Just using socket.id as the username for now and the first one is the leader;
+    let userId = socket.id;
+    game.playerJoined({
+      uid: userId,
+      sid: socket.id,
+    });
+    if (game.leader == null) {
+      console.log('GAME LEADER SET TO ' + userId);
+      game.leader = userId;
+    }
+    io.emit('user-joined', {
+      uid: userId,
+      sid: socket.id,
+    });
+    io.to(socket.id).emit('status', game.getStatus(userId));
 
-module.exports = function(app, io){
-	var game = new GoFish();
-	io.on('connection', function(socket){
-		// Just using socket.id as the username for now and the first one is the leader;
-		var userId = socket.id
-	    game.playerJoined({
-		    uid: userId,
-		    sid: socket.id
-		});
-	    if (game.leader == null) {
-		    console.log("GAME LEADER SET TO "+userId);
-		    game.leader = userId;
-	    }
-	    io.emit("user-joined", {
-		    uid: userId,
-		    sid: socket.id
-	    });
-	    io.to(socket.id).emit("status", game.getStatus(userId));
-	    
-	    socket.on("start-game", function(abc) {
-			game.startGame();
-			for (var i in game.players) {
-				var player = game.players[i];
-				io.to(player.sid).emit("game-state", game.getStateFor(player.uid));
-			}
-			io.emit("players-turn", game.pTurn);
-	    });
-	    socket.on("ask-for", function(ask) {
-		    var res = game.goFish(ask);
-		    var result = res["result"];
-		    var resultFrom = ask.asks;
-		    io.to(socket.id).emit("game-state", game.getStateFor(ask.uid));
-		    io.emit("game-info", {player: userId, message: ask.uid+", do you have any "+ask.asksFor+"'s?"});
-		    if (result != "Go Fish") {
-			    console.log("UPDATE OTHERS HAND");
-			    io.to(game.players[ask.asks].sid).emit("game-state", game.getStateFor(ask.asks));
-			    
-		    }
-		    io.emit("game-info", {player: ask.asks, message: result});
-		    io.emit("players-turn", game.pTurn);
-		    if (res["books"].length != 0) {
-			    io.emit("player-books", {player: userId, gotBooks: res["books"]});
-		    }
-	    });
-	    
-	    
-	    
-	});
-
-}
+    socket.on('start-game', function(abc) {
+      game.startGame();
+      for (let i in game.players) {
+        let player = game.players[i];
+        io.to(player.sid).emit('game-state', game.getStateFor(player.uid));
+      }
+      io.emit('players-turn', game.pTurn);
+    });
+    socket.on('ask-for', function(ask) {
+      let res = game.goFish(ask);
+      let result = res['result'];
+      let resultFrom = ask.asks;
+      io.to(socket.id).emit('game-state', game.getStateFor(ask.uid));
+      io.emit('game-info', {
+        player: userId,
+        message: ask.uid + ', do you have any ' + ask.asksFor + '\'s?',
+      });
+      if (result != 'Go Fish') {
+        console.log('UPDATE OTHERS HAND');
+        io
+          .to(game.players[ask.asks].sid)
+          .emit('game-state', game.getStateFor(ask.asks));
+      }
+      io.emit('game-info', { player: ask.asks, message: result });
+      io.emit('players-turn', game.pTurn);
+      if (res['books'].length != 0) {
+        io.emit('player-books', { player: userId, gotBooks: res['books'] });
+      }
+    });
+  });
+};
