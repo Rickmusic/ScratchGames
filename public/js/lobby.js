68213e069b1324cc41765ae382a77065d635e772
Austin Wattling <Austin Wattling@CINDY>
2018-03-23 20:29:03 -0600

    More work on lobby landing page. Things starting to get dynamic. Lots of work left.

--- public/js/lobby.js
+++ public/js/lobby.js
@@ -2,12 +2,113 @@
 Scratch.lobby = function() {};
 
 (function() {
+  let socket = Scratch.sockets.base; /* Links to app/socket/base.js */
   Scratch.lobby.init = function() {
     // We also run this after a game has terminated // Can choose to start another game etc//
-    function loadLobby() {
-      // TODO tell UI what game settings to load and other information we should have now  //
-      // Perhaps another js file but if we can avoid it till we can load game specific js.
-      //
+    // Organized top left down right loading...//
+    // Assume there is a call to server here to get //
+    // Lobby name
+    // Gametype
+    // Joincode
+    // If Im host, player or spec //
+    // What username I am //
+
+    // TEMP VALUES TO WORK WITH //
+    let gameType = 'Go Fish';
+    let lobbyName = 'Pizza';
+    let joincode = '75843';
+    let role = 'player';
+
+    //Top bar information //
+    $('#gameType')
+      .children()
+      .append(' ' + gameType);
+    $('#lobbyName')
+      .children()
+      .append(' ' + lobbyName);
+    $('#joincode')
+      .children()
+      .append(' ' + joincode);
+
+    // Load member lists //
+
+
+    // Load game specific settings //
+    switch (gameType) {
+      case 'Go Fish':
+        $('#gameSettings').load('snippets/goFishSettings.html');
+        break;
+      case 'UNO':
+        $('#gameSettings').append('SPECIFIC GAME SETTINGS NOT FOUND');
+        break;
+    }
+    // This will disable the newly loaded in form. Must be called after this whole function has run for not host. TODO
+    /*$('#gameSet :input').prop('disabled', true);*/
+
+    /*Load Danger Zone settings */
+    // Hook submit change //
+    $('#editLobby').submit(function(e) {
+      e.preventDefault();
+      e.stopImmediatePropagation();
+      if (
+        confirm(
+          'These changes will un-ready players and could potentially kick players from the lobby. Do you wish to continue?'
+        )
+      ) {
+        let data = $(this)
+          .closest('form')
+          .serializeJSON();
+        // TODO Form valid and filled out ? //
+        socket.emit('edit lobby', data);
+        // TODO We should wait for server to create the lobby in case validation failed,
+        //      or some other feedback needs to be given.
+        this.reset(); // Clear form input (or maybe we shouldn't incase there is an error)
+        // Currently server calls nav upon success. Can be changed if we want to manage UI here.
+      } else {
+        // Do nothing //
+      }
+    });
+    /* Ask for game types from server */
+    socket.emit('game types', {});
+
+    socket.on('game types', function(data) {
+      $('#editLobby select[name="gametype"]')
+        .empty()
+        .append('<option value="" disabled selected>Select</option>');
+      for (let i = 0; i < data.GameTypes.length; i++) {
+        let gametype = data.GameTypes[i];
+        $('#editLobby select[name="gametype"]').append(
+          $('<option>')
+            .val(gametype.id)
+            .data('minPlay', gametype.minPlayer)
+            .data('maxPlay', gametype.maxPlayer)
+            .text(gametype.display)
+        );
+      }
+    });
+
+    $('#editLobby select[name="gametype"]').change(function() {
+      $('#numPlay').prop('disabled', false);
+      $('#confirm').prop('disabled', false);
+      $('#editLobby select[name="numPlayers"] option:disabled').prop(
+        'selected',
+        true
+      );
+      $('#editLobby select[name="numPlayers"] option:enabled').remove();
+      let $opt = $(this).find('option:selected');
+      for (let i = $opt.data('minPlay'); i <= $opt.data('maxPlay'); i++) {
+        $('#editLobby select[name="numPlayers"]').append(
+          $('<option>')
+            .val(i)
+            .text(i)
+        );
+      }
+    });
+
+    if(role != "host"){
+      $('#startBtn').html("Ready Up");
+    }else{
+      $('#startBtn').prop('disabled', true);
     }
   };
 })();
