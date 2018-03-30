b7f7743a91ba41f796cc9a3fd6b12029faf53279
Kayleigh Kernaghan <26681805+AylaJK@users.noreply.github.com>
2018-03-30 14:37:24 -0600

    Linting Uno Files

--- app/games/uno/game.js
+++ app/games/uno/game.js
@@ -1,188 +1,202 @@
-var Deck = require("../deck");
-var Player = require("../player");
+let Deck = require('../deck');
+let Player = require('../player');
 class Uno {
-	constructor() {
-		this.gameStarted = false;
-		this.pTurn = -1;
-		this.direction = 1;
-		this.players = {},
-		this.leader = null,
-		this.deck = new Deck();
-		this.playedCards = [];
-		this.playedSuit = null;
-		this.playedNum = null;
-	}
-	get numberPlayers() {
-		return Object.keys(this.players).length;
-	}
-	get numberCardsToDeal() {
-		return 8;
-	}
-	get lastCard() {
-		if (this.playedCards.length == 0) {
-			return null;
-		}
-		return this.playedCards[this.playedCards.length-1];
-	}
-	startGame() {
-		this.gameStarted = true;
-		var playersCards = this.deck.dealCards(this.numberCardsToDeal, this.numberPlayers);
-		var counter = 0;
-		for (var i in this.players) {
-			
-			this.players[i].setHand(playersCards[counter]);
-			counter++;
-		}
-		var firstCard = this.deck.pickOne()
-		this.playedCards.push(firstCard);
-		this.playedSuit = firstCard.suit;
-		this.playedNum = firstCard.num;
-		this.pTurn = Object.keys(this.players)[Math.floor(Math.random()*this.numberPlayers)];
-	}
-	playerJoined(player) {
-		var pl = new Player(player);
-		this.players[player.uid] = pl;
-	}
-	getStatus(player) {
-		if (!this.gameStarted) {
-			return {
-				players: this.players,
-				leader: this.leader,
-				turn: this.pTurn,
-				allowed: {suit: this.playedSuit, num: this.playedNum},
-				lastCard: this.lastCard,
-			}
-		}
-	}
-	getStateFor(uid) {
-		var returnData = {players: {}, allowed: {
-			suit: this.playedSuit, 
-			num: this.playedNum
-		}, lastCard: this.lastCard};
-		console.log(returnData);
-		for (var i in this.players) {
-			var player = this.players[i];
-			if (player.uid == uid) {
-				returnData["players"][player.uid] = player;
-			}
-			else {
-				returnData["players"][player.uid] = {
-					uid: player.uid,
-					sid: player.sid,
-					hand: player.hand.length
-				}
-			}
-		}
-		return returnData;
-	}
-	nextTurn() {
-		for (var i in this.players) {
-			this.pTurn = this.nextPlayerTurn();
-			console.log(this.pTurn);
-			if (this.curPlayer.hand.length > 0 || this.deck.cardsAvailable.length > 0) {
-				return false;
-			}
-		}
-		// There are no cards in the deck, and no players with cards
-		return true;
-		
-	}
-	get curPlayer() {
-		var curTurnNum = Object.keys(this.players).indexOf(this.pTurn);
-		var playerIds = Object.keys(this.players);
-		return this.players[this.pTurn];
-	}
-	get nextPlayer() {
-		return this.players[this.nextPlayerTurn()];
-	}
-	nextPlayerTurn() {
-		var curTurnNum = Object.keys(this.players).indexOf(this.pTurn);
-		var playerIds = Object.keys(this.players);
-		var incTurnNum = (curTurnNum+this.direction);
-		console.log(incTurnNum);
-		var nextTurnNum;
-		if (incTurnNum >= 0) {
-			nextTurnNum = incTurnNum%this.numberPlayers;
-		}
-		else {
-			nextTurnNum = this.numberPlayers+incTurnNum;
-		}
-		return playerIds[nextTurnNum];
-	}
-	giveCards(player, numCards) {
-		var counter = 0;
-		while (counter < numCards) {
-			if (this.deck.cardsAvailable.length == 0) {
-				// Re-shuffle??
-				this.deck.cardsAvailable = this.playedCards.slice(0, this.playedCards.length-1);
-				this.playedCards = this.playedCards.slice(this.playedCards.length-1, this.playedCards.length);
-			}
-			player.giveCard(this.deck.pickOne());
-			counter += 1;
-		}
-		
-	}
-	placedCard(card, user, options) {
-		var player = this.players[user];
-		var options;
-		console.log(card);
-		if (card == null) {
-			player.giveCard(this.deck.pickOne());
-			return true;
-		}
-		if (card.num == 1) {
-			// Change suit + 4 cards for next player
-			this.playedSuit = options["suit"];
-			this.playedNum = null;
-			var next = this.nextPlayer;
-			
-			this.giveCards(next, 4);
-		}
-		else if (card.num == 13) {
-			this.playedSuit = options["suit"];
-			this.playedNum = null;
-			// Change suit
-		}
-		else if (card.num == 12) {
-			// Give 2
-			var next = this.nextPlayer;
-			this.giveCards(next, 2);
-			this.playedSuit = card.suit;
-			this.playedNum = card.num;
-		}
-		else if (card.num == 11) {
-			// Skip or swap direction
-			console.log(options);
-			this.direction *= -1;
-			this.playedSuit = card.suit;
-			this.playedNum = card.num;
-		}
-		else {
-			if (card.suit != this.playedSuit && card.num != this.playedNum) {
-				return false
-			}
-			this.playedSuit = card.suit;
-			this.playedNum = card.num;
-		}
-		this.curPlayer.takeCardOfSuit(card.num, card.suit);
-		this.playedCards.push(card);
-		this.nextTurn();
-		return true;
-	}
-	getWinner() {
-		var maxBooks = 0;
-		var winners = [];
-		for (var i in this.players) {
-			if (this.players[i].books.length == maxBooks) {
-				this.winners.push(this.players[i].uid);
-			}
-			else if (this.players[i].books.length > maxBooks) {
-				this.winners = [this.players[i].uid];
-			}
-		}
-		return winners;
-	}
-	
-	
+  constructor() {
+    this.gameStarted = false;
+    this.pTurn = -1;
+    this.direction = 1;
+    this.players = {};
+    this.leader = null;
+    this.deck = new Deck();
+    this.playedCards = [];
+    this.playedSuit = null;
+    this.playedNum = null;
+  }
+  get numberPlayers() {
+    return Object.keys(this.players).length;
+  }
+  get numberCardsToDeal() {
+    return 8;
+  }
+  get lastCard() {
+    if (this.playedCards.length == 0) {
+      return null;
+    }
+    return this.playedCards[this.playedCards.length - 1];
+  }
+  startGame() {
+    this.gameStarted = true;
+    let playersCards = this.deck.dealCards(
+      this.numberCardsToDeal,
+      this.numberPlayers
+    );
+    let counter = 0;
+    for (let i in this.players) {
+      this.players[i].setHand(playersCards[counter]);
+      counter++;
+    }
+    let firstCard = this.deck.pickOne();
+    this.playedCards.push(firstCard);
+    this.playedSuit = firstCard.suit;
+    this.playedNum = firstCard.num;
+    this.pTurn = Object.keys(this.players)[
+      Math.floor(Math.random() * this.numberPlayers)
+    ];
+  }
+  playerJoined(player) {
+    let pl = new Player(player);
+    this.players[player.uid] = pl;
+  }
+  getStatus(player) {
+    if (!this.gameStarted) {
+      return {
+        players: this.players,
+        leader: this.leader,
+        turn: this.pTurn,
+        allowed: { suit: this.playedSuit, num: this.playedNum },
+        lastCard: this.lastCard,
+      };
+    }
+    return null;
+  }
+  getStateFor(uid) {
+    let returnData = {
+      players: {},
+      allowed: {
+        suit: this.playedSuit,
+        num: this.playedNum,
+      },
+      lastCard: this.lastCard,
+    };
+    console.log(returnData);
+    for (let i in this.players) {
+      let player = this.players[i];
+      if (player.uid == uid) {
+        returnData['players'][player.uid] = player;
+      }
+      else {
+        returnData['players'][player.uid] = {
+          uid: player.uid,
+          sid: player.sid,
+          hand: player.hand.length,
+        };
+      }
+    }
+    return returnData;
+  }
+  nextTurn() {
+    for (let i in this.players) {
+      this.pTurn = this.nextPlayerTurn();
+      console.log(this.pTurn);
+      if (
+        this.curPlayer.hand.length > 0 ||
+        this.deck.cardsAvailable.length > 0
+      ) {
+        return false;
+      }
+    }
+    // There are no cards in the deck, and no players with cards
+    return true;
+  }
+  get curPlayer() {
+    let curTurnNum = Object.keys(this.players).indexOf(this.pTurn);
+    let playerIds = Object.keys(this.players);
+    return this.players[this.pTurn];
+  }
+  get nextPlayer() {
+    return this.players[this.nextPlayerTurn()];
+  }
+  nextPlayerTurn() {
+    let curTurnNum = Object.keys(this.players).indexOf(this.pTurn);
+    let playerIds = Object.keys(this.players);
+    let incTurnNum = curTurnNum + this.direction;
+    console.log(incTurnNum);
+    let nextTurnNum;
+    if (incTurnNum >= 0) {
+      nextTurnNum = incTurnNum % this.numberPlayers;
+    }
+    else {
+      nextTurnNum = this.numberPlayers + incTurnNum;
+    }
+    return playerIds[nextTurnNum];
+  }
+  giveCards(player, numCards) {
+    let counter = 0;
+    while (counter < numCards) {
+      if (this.deck.cardsAvailable.length == 0) {
+        // Re-shuffle??
+        this.deck.cardsAvailable = this.playedCards.slice(
+          0,
+          this.playedCards.length - 1
+        );
+        this.playedCards = this.playedCards.slice(
+          this.playedCards.length - 1,
+          this.playedCards.length
+        );
+      }
+      player.giveCard(this.deck.pickOne());
+      counter += 1;
+    }
+  }
+  placedCard(card, user, options) {
+    let player = this.players[user];
+    var options;
+    console.log(card);
+    if (card == null) {
+      player.giveCard(this.deck.pickOne());
+      return true;
+    }
+    if (card.num == 1) {
+      // Change suit + 4 cards for next player
+      this.playedSuit = options['suit'];
+      this.playedNum = null;
+      var next = this.nextPlayer;
+
+      this.giveCards(next, 4);
+    }
+    else if (card.num == 13) {
+      this.playedSuit = options['suit'];
+      this.playedNum = null;
+      // Change suit
+    }
+    else if (card.num == 12) {
+      // Give 2
+      var next = this.nextPlayer;
+      this.giveCards(next, 2);
+      this.playedSuit = card.suit;
+      this.playedNum = card.num;
+    }
+    else if (card.num == 11) {
+      // Skip or swap direction
+      console.log(options);
+      this.direction *= -1;
+      this.playedSuit = card.suit;
+      this.playedNum = card.num;
+    }
+    else {
+      if (card.suit != this.playedSuit && card.num != this.playedNum) {
+        return false;
+      }
+      this.playedSuit = card.suit;
+      this.playedNum = card.num;
+    }
+    this.curPlayer.takeCardOfSuit(card.num, card.suit);
+    this.playedCards.push(card);
+    this.nextTurn();
+    return true;
+  }
+  getWinner() {
+    let maxBooks = 0;
+    let winners = [];
+    for (let i in this.players) {
+      if (this.players[i].books.length == maxBooks) {
+        this.winners.push(this.players[i].uid);
+      }
+      else if (this.players[i].books.length > maxBooks) {
+        this.winners = [this.players[i].uid];
+      }
+    }
+    return winners;
+  }
 }
 module.exports = Uno;
--- app/games/uno/index.js
+++ app/games/uno/index.js
@@ -1,47 +1,45 @@
-var Uno = require("./game");
+let Uno = require('./game');
 
+module.exports = function(app, io) {
+  let game = new Uno();
+  io.on('connection', function(socket) {
+    // Just using socket.id as the username for now and the first one is the leader;
+    let userId = socket.id;
+    game.playerJoined({
+      uid: userId,
+      sid: socket.id,
+    });
+    if (game.leader == null) {
+      console.log('GAME LEADER SET TO ' + userId);
+      game.leader = userId;
+    }
+    io.emit('user-joined', {
+      uid: userId,
+      sid: socket.id,
+    });
+    io.to(socket.id).emit('status', game.getStatus(userId));
 
-
-module.exports = function(app, io){
-	var game = new Uno();
-	io.on('connection', function(socket){
-		// Just using socket.id as the username for now and the first one is the leader;
-		var userId = socket.id
-	    game.playerJoined({
-		    uid: userId,
-		    sid: socket.id
-		});
-	    if (game.leader == null) {
-		    console.log("GAME LEADER SET TO "+userId);
-		    game.leader = userId;
-	    }
-	    io.emit("user-joined", {
-		    uid: userId,
-		    sid: socket.id
-	    });
-	    io.to(socket.id).emit("status", game.getStatus(userId));
-	    
-	    socket.on("start-game", function(abc) {
-			game.startGame();
-			for (var i in game.players) {
-				var player = game.players[i];
-				io.to(player.sid).emit("game-state", game.getStateFor(player.uid));
-			}
-			io.emit("players-turn", game.pTurn);
-	    });
-	    socket.on("placed-card", function(place) {
-		    var res = game.placedCard(place["card"], userId, place["options"]);
-		    if (res) {
-			    for (var i in game.players) {
-				    var player = game.players[i];
-				    io.to(player.sid).emit("game-state", game.getStateFor(player.uid));
-			    }
-			    io.emit("players-turn", game.pTurn);
-		    }
-		    else {
-			    // Display an error message
-		    }
-		    /*var result = res["result"];
+    socket.on('start-game', function(abc) {
+      game.startGame();
+      for (let i in game.players) {
+        let player = game.players[i];
+        io.to(player.sid).emit('game-state', game.getStateFor(player.uid));
+      }
+      io.emit('players-turn', game.pTurn);
+    });
+    socket.on('placed-card', function(place) {
+      let res = game.placedCard(place['card'], userId, place['options']);
+      if (res) {
+        for (let i in game.players) {
+          let player = game.players[i];
+          io.to(player.sid).emit('game-state', game.getStateFor(player.uid));
+        }
+        io.emit('players-turn', game.pTurn);
+      }
+      else {
+        // Display an error message
+      }
+      /*var result = res["result"];
 		    if (result == "Game Over") {
 			    var winner = game.getWinner();
 			    return io.emit("game-over", winner)
@@ -59,10 +57,6 @@ module.exports = function(app, io){
 		    if (res["books"].length != 0) {
 			    io.emit("player-books", {player: userId, gotBooks: res["books"]});
 		    }*/
-	    });
-	    
-	    
-	    
-	});
-
-}
+    });
+  });
+};
